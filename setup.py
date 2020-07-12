bea82b8edb87b44e4445fe137ce4fa33a5a7fed8
Vidhi Bagadia <vidbagadia@gmail.com>
2020-07-12 16:50:28 +0530

    setting up pkg

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 setup(
-    name='box',
+    name='dipor',
     version='1.0',
-    py_modules=['dipor/md_extensions'],
-    install_requires = ['markdown>=3.0'],
+    description='Static Site Generator written in Python for ease of processes',
+    url="https://github.com/vidhibagadia/dipor",
+    author="Vidh Bagadia",
+    author_email="vidbagadia@gmail.com",
+    license="MIT",
+    packages=['dipor'],
+    install_requires = ['markdown==3.2.2', 'Jinja2==2.11.2', 'html5print==0.1.2', 'Pygments==2.6.1'],
 )
