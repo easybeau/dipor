d792969af7955c37ff01f757987a315996bcded0
Vidhi Bagadia <vidbagadia@gmail.com>
2020-07-12 18:17:28 +0530

    pkg chanes

--- setup.py
+++ setup.py
@@ -7,6 +7,17 @@ setup(
     author="Vidh Bagadia",
     author_email="vidbagadia@gmail.com",
     license="MIT",
+    keywords='static site generator staticsitegenerator python jinja',
+    classifiers=[
+        'Development Status "" 3 - Alpha',
+        'Lcense :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ]
     packages=['dipor'],
     install_requires = ['markdown==3.2.2', 'Jinja2==2.11.2', 'html5print==0.1.2', 'Pygments==2.6.1'],
+    entry_points={
+        'console_scripts': [
+            'dipor=dipor.main:main'
+        ]
+    }
 )
