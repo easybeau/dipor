5fcca5cf281449e5b3f08b4b0d86394db9e26642
Vidhi Bagadia <vidbagadia@gmail.com>
2020-07-12 21:07:02 +0530

    trying start

--- dipor/cmd/start.py
+++ dipor/cmd/start.py
@@ -2,7 +2,7 @@ import pathlib
 import shutil
 import os
 import dipor
-from dipor import main
+from dipor.main import mainap
 
 def main():
     # src_root = pathlib.Path(__file__).parent.absolute()
@@ -10,4 +10,4 @@ def main():
     src_root = os.path.dirname(dipor.__file__)
     shutil.copytree(os.path.join(src_root, 'content'), os.path.join(dest_root, 'content'))
     shutil.copytree(os.path.join(src_root, 'src'), os.path.join(dest_root, 'src'))
-    main.mainap()
+    mainap()
