4c3b938f8d678a0c7025b0a9e2d08f318246247f
Vidhi Bagadia <vidbagadia@gmail.com>
2020-07-12 19:41:17 +0530

    check flder move

--- dipor/cmd/start.py
+++ dipor/cmd/start.py
@@ -7,4 +7,6 @@ def main():
     # src_root = pathlib.Path(__file__).parent.absolute()
     dest_root = pathlib.Path().absolute()
     src_root = os.path.dirname(dipor.__file__)
+    print(os.path.join(src_root, 'src'))
+    print(dest_root)
     shutil.copytree(os.path.join(src_root, 'src'), dest_root)
