8248f5cb23f9d872ebac0a82915971e99604b5c2
Vidhi Bagadia <vidbagadia@gmail.com>
2020-07-12 18:21:27 +0530

    pkg chanes

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         'Development Status "" 3 - Alpha',
         'Lcense :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
-    ]
+    ],
     packages=['dipor'],
     install_requires = ['markdown==3.2.2', 'Jinja2==2.11.2', 'html5print==0.1.2', 'Pygments==2.6.1'],
     entry_points={
