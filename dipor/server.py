515c7300e6fe74e67b619a1bebb202a2c155a2f8
Vidhi Bagadia <vidbagadia@gmail.com>
2020-07-18 19:08:53 +0530

    server fix

--- dipor/server.py
+++ dipor/server.py
@@ -11,12 +11,12 @@ def runserver(public_folder):
             self.public = public_folder
 
         def do_GET(self):
-        path = self.path.strip('/')
-        for file in listdir(self.public):
-            if file == path and os.path.isdir(os.path.join(self.public, file)):
-                self.path = os.path.join(self.public, file, "index.html")
+            path = self.path.strip('/')
+            for file in listdir(self.public):
+                if file == path and os.path.isdir(os.path.join(self.public, file)):
+                    self.path = os.path.join(self.public, file, "index.html")
 
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+            return http.server.SimpleHTTPRequestHandler.do_GET(self)
     handler = CustomHttpRequestHandler
     socketserver.TCPServer.allow_reuse_address = True
 
