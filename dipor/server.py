3a8c9c11792a9a6d1bca08532943f22ffd6ce102
Vidhi Bagadia <vidbagadia@gmail.com>
2020-07-18 20:31:23 +0530

    fixes

--- dipor/server.py
+++ dipor/server.py
@@ -12,16 +12,11 @@ def runserver(public_folder):
             super(CustomHttpRequestHandler, self).__init__(*args, **kwargs)
             
         def do_GET(self):
-            print(self.public)
             path = self.path.strip('/')
-            print(path)
-            if path == "":
-                self.path = os.path.join(self.public, 'index.html')
-            else:
-                for file in listdir(self.public):
-                    print(file)
-                    if file == path and os.path.isdir(os.path.join(self.public, file)):
-                        self.path = os.path.join(self.public, file, "index.html")
+            for file in listdir(self.public):
+                if file == path and os.path.isdir(os.path.join(self.public, file)):
+                    print(path, "-->", os.path.join(self.public, file, "index.html"))
+                    self.path = os.path.join(self.public, file, "index.html")
 
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
     handler = CustomHttpRequestHandler
