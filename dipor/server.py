a2f1a9ee3fa067b4785d6e29a2f52a8a6096b845
Vidhi Bagadia <vidbagadia@gmail.com>
2020-07-19 00:17:00 +0530

    fixz

--- dipor/server.py
+++ dipor/server.py
@@ -22,9 +22,10 @@ def runserver():
 
     with socketserver.TCPServer(("", PORT), handler) as httpd:
         print("server is revving up...")
-        httpd.serve_forever()
         print("woohoo! check out this awesome page @ localhost:5050")
         print("you can keep track of your requests here")
         print("see you on the actual url next time :)")
+        webbrowser.open(f"http://localhost:{PORT}")
+        httpd.serve_forever()
 
 
