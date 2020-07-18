0d9d20e72cc2f63f5dc8e5b1ade5b0e736ac66b7
Vidhi Bagadia <vidbagadia@gmail.com>
2020-07-18 21:54:02 +0530

    fixz

--- dipor/server.py
+++ dipor/server.py
@@ -11,12 +11,17 @@ def runserver():
     class CustomHttpRequestHandler(http.server.SimpleHTTPRequestHandler):            
         def do_GET(self):
             path = self.path.strip('/')
+            print(path)
             if path == "":
+                print("1")
                 self.path = os.path.join(web_dir, 'index.html')
             else:
+                print("2")
                 if os.path.isfile(os.path.join(web_dir, path+".html")):
+                    print(web_dir, path, ".html")
                     self.path = os.path.join(web_dir, path+".html")
                 elif os.path.isfile(os.path.join(web_dir, path, "index.html")):
+                    print(web_dir, path, "index.html")
                     self.path = os.path.join(web_dir, path, "index.html")
                 
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
